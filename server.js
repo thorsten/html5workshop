ae51c69349bb937e192557b270f42b33acf6b570
Sebastian Springer <sebastian.springer@mayflower.de>
2013-02-28 17:47:42 +0100

    query database tables

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.get('/articles', function (req, res) {
     });
 });
 
+app.get('/customers', function (req, res) {
+    db.all('SELECT rowid, * FROM `customers`', function (err, rows) {
+        if (err) throw err;
+        res.send(rows);
+    });
+});
+
+app.get('/orders', function (req, res) {
+    db.all('select * from orders LEFT JOIN customer ON customer.rowid = orders.customer_id LEFT JOIN article ON article.rowid = orders.article_id', function (err, rows) {
+        if (err) throw err;
+        res.send(rows);
+    });
+});
+
 
 app.use("/js", express.static(__dirname + '/js'));
 
